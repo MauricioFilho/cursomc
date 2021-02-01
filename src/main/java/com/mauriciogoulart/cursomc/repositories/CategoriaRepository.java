248f83dba91cbc6920fc1fde9c848605c2714b90
Mauricio Goulart <mauriciogoulart23@live.com>
2021-02-01 12:53:00 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/mauriciogoulart/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.mauriciogoulart.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mauriciogoulart.cursomc.domain.Categoria;
+
+@Repository 
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+
+}
